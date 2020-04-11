2fc4dd7b774174e855defee3f0c36057dd86dd42
Anthony Liu <a@anthony.ai>
2020-04-11 14:37:16 -0700

    Add eslint.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
-export default function Index() {
+export default function Index () {
   return (
     <div>
-      <p>Quarantine game</p>
+      <div className="a">A</div>
+      <div className="b">B</div>
+      <div className="c">C</div>
+
+      <style jsx>{`
+      .a {
+        background: white;
+      }
+
+      .b {
+        background: red;
+      }
+
+      .c {
+        background: brown;
+      }
+      `}</style>
+
+      <GlobalStyles />
     </div>
+  );
+}
+
+function GlobalStyles () {
+  return (
+    <style jsx global>{`
+      html, body {
+        margin: 0;
+        padding: 0;
+      }
+    `}</style>
   );
 }
